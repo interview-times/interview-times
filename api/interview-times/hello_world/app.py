8497cc51ec602afc0fa0ed74a77524a662f99c50
kazuwsx <kazu.wsx@gmail.com>
2024-12-15 00:44:48 +0900

    返答API作成

--- api/interview-times/hello_world/app.py
+++ api/interview-times/hello_world/app.py
@@ -1,11 +1,27 @@
 import json
+import os
+from openai import OpenAI
 
 def lambda_handler(event, context):
     answer = event.get('queryStringParameters').get('answer')
+    env = os.environ['OPENAI_API_KEY']
+    client = OpenAI(api_key=env)
 
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": answer
+            }
+        ]
+    )
+    print(completion.choices[0].message.content)
+    
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "answer": answer,
-        }),
+            "message": completion.choices[0].message.content,
+        }, ensure_ascii=False),
     }
