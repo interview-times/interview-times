d866c2ea4e9b84d35aa007ed4987e1a265678f26
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-12-15 09:42:04 +0900

    結果画面にとりあえずデータを表示できるようにした

--- front/src/app/result/page.tsx
+++ front/src/app/result/page.tsx
@@ -5,76 +5,68 @@ import { useArticleResultData } from "@/contexts/article-result-data.context";
 export default function Result() {
   const { articleResultData } = useArticleResultData();
 
-    // if (!articleResultData) {
-    //   return <div>データがありません</div>;
-    // }
+  if (!articleResultData) {
+    return <div>データがありません</div>;
+  }
 
-    return (
-        <div className="flex justify-center">
-            <div className="lg:container mt-6 p-8">
-                <input
-                    type="text"
-                    value='タイトル'
-                    className="text-2xl mb-7 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
-                <input
-                    type="text"
-                    value='さぶタイトル'
-                    className="text-xl mb-9 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                />
-                <div className="rounded-md border-2 border-sky-800 mt-3 mb-9 p-6">
-                    <div className="text-sky-900 text-xl font-bold  mb-2">
-                        きっかけ
-                    </div>
-                    <div className=" text-sm text-sky-900 font-bold mb-3">
-                        事業や商品の始まったきっかけを書きます
-                    </div>
-                    <input
-                        type="text"
-                        value=''
-                        className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
-                <div className="rounded-md border-2 border-sky-800 mb-9 p-6">
-                    <div className="text-sky-900 text-xl font-bold  mb-2">
-                        危機
-                    </div>
-                    <div className=" text-sm text-sky-900 font-bold mb-3">
-                        会社や商品開発でつまづいた危機を書きます
-                    </div>
-                    <input
-                        type="text"
-                        value=''
-                        className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
-                <div className="rounded-md border-2 border-sky-800 mb-9 p-6">
-                    <div className="text-sky-900 text-xl font-bold  mb-2">
-                        転期
-                    </div>
-                    <div className=" text-sm text-sky-900 font-bold mb-3">
-                        危機を乗り越えるこえた転期を
-                    </div>
-                    <input
-                        type="text"
-                        value=''
-                        className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
-                <div className="rounded-md border-2 border-sky-800 p-6">
-                    <div className="text-sky-900 text-xl font-bold  mb-2">
-                        成果
-                    </div>
-                    <div className=" text-sm text-sky-900 font-bold mb-3">
-                        危機を乗り越えるこえた転期を
-                    </div>
-                    <input
-                        type="text"
-                        value=''
-                        className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                    />
-                </div>
-            </div>
+  return (
+    <div className="flex justify-center">
+      <div className="lg:container mt-6 p-8">
+        <input
+          type="text"
+          value="タイトル"
+          className="text-2xl mb-7 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+        <input
+          type="text"
+          value="さぶタイトル"
+          className="text-xl mb-9 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        />
+        <div className="rounded-md border-2 border-sky-800 mt-3 mb-9 p-6">
+          <div className="text-sky-900 text-xl font-bold  mb-2">きっかけ</div>
+          <div className=" text-sm text-sky-900 font-bold mb-3">
+            事業や商品の始まったきっかけを書きます
+          </div>
+          <input
+            type="text"
+            value={articleResultData.content}
+            className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
         </div>
-    );
+        <div className="rounded-md border-2 border-sky-800 mb-9 p-6">
+          <div className="text-sky-900 text-xl font-bold  mb-2">危機</div>
+          <div className=" text-sm text-sky-900 font-bold mb-3">
+            会社や商品開発でつまづいた危機を書きます
+          </div>
+          <input
+            type="text"
+            value=""
+            className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div className="rounded-md border-2 border-sky-800 mb-9 p-6">
+          <div className="text-sky-900 text-xl font-bold  mb-2">転期</div>
+          <div className=" text-sm text-sky-900 font-bold mb-3">
+            危機を乗り越えるこえた転期を
+          </div>
+          <input
+            type="text"
+            value=""
+            className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div className="rounded-md border-2 border-sky-800 p-6">
+          <div className="text-sky-900 text-xl font-bold  mb-2">成果</div>
+          <div className=" text-sm text-sky-900 font-bold mb-3">
+            危機を乗り越えるこえた転期を
+          </div>
+          <input
+            type="text"
+            value=""
+            className="text-xl mb-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
