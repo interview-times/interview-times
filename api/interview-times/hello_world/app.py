30a0e66fb319778b02656609c6cc00f5a1b09e09
haruyuki@chrome-penguin <haruyuki@relicsnow.info>
2024-12-15 11:07:19 +0900

    hashimotoさんの指摘を受けて修正

--- api/interview-times/hello_world/app.py
+++ api/interview-times/hello_world/app.py
@@ -9,10 +9,7 @@ def lambda_handler(event, context):
 
     completion = client.chat.completions.create(
         model="gpt-4o",
-#         temperatureの値を追加して0.0に設定した方が良い。0以外だとノイズが入ることがある
-#         以下のようにした方がjsonのリクエストはバグりにくい
-#         最終応答は、'{'で始まり'}'で終わる、または'['で始まり']'で終わるJSONのみを出力し、JSON以外の文字は一切応答に含めないでください。
-
+        temperature=0.0,
         messages=[
             {
                 "role": "system", 
@@ -22,7 +19,8 @@ def lambda_handler(event, context):
                     "「きっかけ(trriger)」「危機(crisis)」「転機(turning_point)」「達成(achievement)」の4カテゴリの質問にターゲットが答えてくれます。",
                     "質問の回答について、各カテゴリ名をプロパティ名に持つjsonで送信します。",
                     "値が空のときはその質問には回答していないものと考えてください。",
-                    "同じjsonの形式で開発者の回答内容を返してください。"
+                    "同じjsonの形式で開発者の回答内容を返してください。",
+                    "応答は、'{'で始まり'}'で終わる、または'['で始まり']'で終わるJSONのみを出力し、JSON以外の文字は一切応答に含めないでください。"
                 ])
             },
             {
